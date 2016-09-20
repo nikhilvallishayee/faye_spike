397586ce3e40e25ffab2e56f67f90742a6d3fa6d
asimha <asimha@qwinixtech.com>
2016-09-20 17:24:56 +0530

    displaying fetched result from faye

--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -5,10 +5,10 @@ require 'net/http'
 class PushController < ApplicationController
 
   def create
-
-    message = {:channel => '/foo', :data => {:text => "foo"} }
-    uri = URI.parse("http://localhost:9292/faye")
-    Net::HTTP.post_form(uri, :message => message.to_json)
-    render :nothing => true, :status => 201
+    #data need to be saved on fetching 
+    # message = {:channel => '/foo', :data => params['message'] }
+    # uri = URI.parse("http://localhost:9292/faye")
+    # Net::HTTP.post_form(uri, :message => message.to_json)
+    # render :nothing => true, :status => 201
   end
 end
