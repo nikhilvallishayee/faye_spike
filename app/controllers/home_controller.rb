9202ebade1c88e2c0457083c33fd6a3aceaf6bcd
disha_suresh <dsuresh@qwinix.io>
2016-09-26 14:32:52 +0530

    Dashboard view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
   def index
     @push = Push.last
   end
+
+  def dashboard1
+  end
+
+  def dashboard2
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post 'push' , to: "push#create" , as: "push"
   get '/home', to: "home#index", as: "home"
+  get '/home2', to: "home#dashboard1", as: "home2"
+  get '/home3', to: "home#dashboard2", as: "home3"
 end
 
